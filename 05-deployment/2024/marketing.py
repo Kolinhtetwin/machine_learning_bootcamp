cfaa826d8590f30b79f4095aa6a2f1ce1de447de
Lin Htet Win (Ben) <124510991+Kolinhtetwin@users.noreply.github.com>
2025-10-26 07:14:45 +0000

    Doing fast api response

---
+++ 05-deployment/2024/marketing.py
@@ -0,0 +1,38 @@
+
+import requests
+
+url = 'http://localhost:9696/predict'
+
+
+customer =  {
+    "gender": "male",
+    "seniorcitizen": 0,
+    "partner": "no",
+    "dependents": "yes",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 6,
+    "monthlycharges": 29.85,
+    "totalcharges": 129.85
+  }
+
+
+response = requests.post(url, json=customer)
+churn = response.json()
+
+print('response = ', churn)
+
+if churn['churn'] >= 0.5:
+    print('send out a email')
+else:
+    print('dont do anything')
